2e380597c329456a5cd5ce6c3f422f7cacc308df
Reiners, Paul <Paul.Reiners@Pearson.com>
2014-06-16 09:31:09 -0500

    Made nicer plot.

--- src/plot_runs_per_game_by_league.R
+++ src/plot_runs_per_game_by_league.R
@@ -1,6 +1,9 @@
 league.data <- read.csv("./data/runs_per_game.csv")
 colnames(league.data) <- c("year", "league", "games", "runs", "runs.per.game")
 
-library(lattice)
-xyplot(runs.per.game ~ year, groups=league.data$league, data = league.data, 
-       auto.key = list(corner = c(0, .98)), cex = 1.5)
+library(ggplot2)
+g <- ggplot(league.data, aes(year, runs.per.game)) 
+summary(g)
+g + geom_point (aes(color=league), size = 3) + 
+        geom_smooth(method="lm") + ylab("runs per game") +
+        scale_x_continuous(breaks=seq(1870, 2010, 20))
