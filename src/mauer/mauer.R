ed94b9af83841a42b613009f9f3653d257db1d4b
Reiners, Paul <Paul.Reiners@Pearson.com>
2014-07-15 13:01:07 -0500

    More smoothing.

--- src/mauer/mauer.R
+++ src/mauer/mauer.R
@@ -3,3 +3,5 @@ past <- mauer[mauer$Year != 2014,]
 summary(past$OPS)
 sd(past$OPS)
 plot(y=mauer$OPS, x=mauer$Year, xlab="Year", ylab="OPS", main="Joe Mauer", type="l")
+library("ggplot2")
+qplot(Year, OPS, data = mauer) + geom_smooth()
